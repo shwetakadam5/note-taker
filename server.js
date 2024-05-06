2e0fdac6d6588174c173443a35a353f173686364
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-05-06 12:14:07 +1000

    Adding the initial files to load the homepage

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
+
+// This view route is a GET route for the homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
