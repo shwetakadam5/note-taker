24773994e648553bf29e6f6aa67a77e4f057c6d6
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-05-06 12:21:32 +1000

    Updating the server port and also checking the env changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require('express');
 const path = require('path');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
@@ -14,6 +15,11 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/secret', (req, res) => {
+    console.log("credential", process.env.USER);
+    res.end();
+})
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
